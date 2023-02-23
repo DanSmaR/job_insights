f7317bf1743b6047bc4de0ae355be55dfed4fa72
Danilo Martins <dansm86@outlook.com>
2023-02-23 10:07:54 -0300

    feat/req06/implemented filter_by_job_type
    function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -61,4 +61,9 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    jobs_filtered = [
+        job
+        for job in jobs
+        if job["job_type"] == job_type
+    ]
+    return jobs_filtered
