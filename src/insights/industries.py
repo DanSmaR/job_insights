f7aa115c970c4f129410796fbc4a28b45cce7026
Danilo Martins <dansm86@outlook.com>
2023-02-23 10:12:14 -0300

    feat/req07/implemented filter_by_industry
    function

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -40,4 +40,9 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    jobs_filtered = [
+        job
+        for job in jobs
+        if job["industry"] == industry
+    ]
+    return jobs_filtered
