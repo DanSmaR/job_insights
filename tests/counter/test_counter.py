33afd6e42671f4c9fa7073239a23d1514a6e9508
Danilo Martins <dansm86@outlook.com>
2023-02-27 11:46:17 -0300

    feat/req10-testing the counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
 def test_counter():
-    pass
+    mock_jobs_file_content = """title,salary,type
+Front end developer,2000,trainee
+Back end developer,3000,full time
+Full stack end developer,4000,full time"""
+
+    with patch("builtins.open", mock_open(read_data=mock_jobs_file_content)):
+        assert count_ocurrences("dummy", "Developer") == 3
+        assert count_ocurrences("dummy", "developer") == 3
